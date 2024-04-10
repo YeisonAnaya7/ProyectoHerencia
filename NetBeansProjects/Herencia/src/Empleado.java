bce7e6fb707183483ee7e4551d27bd45660cd524
Yeison Andres <Yeisonandresanaya7@gmail.com>
2024-04-09 23:03:27 -0500

    Ejemplo POO repositorios

---
+++ NetBeansProjects/Herencia/src/Empleado.java
@@ -0,0 +1,14 @@
+package Herencia;
+
+public class Empleado extends Persona{
+    
+private double sueldo;
+
+public Empleado(String nombre,String apellido,String documento, double sueldo){
+    super(nombre,apellido,documento);
+    this.sueldo= sueldo;
+}
+public String mostrarEmpleado(){
+    return getNombre()+"Apellido/n"+getApellido()+"Documento/n"+getDocumento()+"sueldo"+sueldo;
+}
+}
